5e4a1dc0854f63a228cd7f7b81590b58a67bc7b8
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2024-08-09 03:49:59 -0400

    vercel fix #2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7f776047247ee642"; }
+/******/ 		__webpack_require__.h = function() { return "b448740df8c1ebca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
