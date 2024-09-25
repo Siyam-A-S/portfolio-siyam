ddb39e6235097f57ecff3149e9b9e433be8125a8
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2024-09-24 20:15:47 -0400

    Updated resume

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -35,11 +35,11 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.usercontent.google.com/download?id=1uKuUFbuQn8fYgYLK-erHHlF0uSrh0O0e&export=download&authuser=1`"
+              href="https://drive.google.com/file/d/1LicxPSE1-6BJFTmilk1338lYqfoT7-G4/view?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+                Download Resume
               </span>
             </Link>
           </div>
